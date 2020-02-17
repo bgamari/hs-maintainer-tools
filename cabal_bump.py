b3cc860d805d70b42b70b4fd7fb53270610fce43
Ben Gamari <ben@smart-cactus.org>
2020-02-17 16:21:40 -0500

    sdist tarball is now in dist-newstyle

--- cabal_bump.py
+++ cabal_bump.py
@@ -275,7 +275,7 @@ def run(mode: str, omit_tag: bool, signing_key: str) -> None:
     print("Release looks good, let's ship it!")
     subprocess.call(["git", "commit", cabal.path, "-m", f"Bump to {new_ver}", "--edit"])
     check_call(["cabal", "sdist"])
-    sdist_tarball = f'dist/{name}-{new_ver}.tar.gz'
+    sdist_tarball = f'dist-newstyle/{name}-{new_ver}.tar.gz'
 
     # Upload
     print_heading('Upload candidate to Hackage')
